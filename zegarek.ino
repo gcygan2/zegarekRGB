342c6191fd60bc7280bdfb2ceb390fae56ff789e
gcygan2 <114261165+gcygan2@users.noreply.github.com>
2026-01-17 13:49:35 +0100

    Update zegarek.ino

--- zegarek.ino
+++ zegarek.ino
@@ -6,8 +6,8 @@
 #define LED_COUNT 8
 
 // WiFi
-const char* ssid     = "TP-LINK_EAD700";
-const char* password = "Skowronek";
+const char* ssid     = "";
+const char* password = "";
 
 // NTP
 const char* ntpServer = "pool.ntp.org";
@@ -16,7 +16,7 @@ const int   daylightOffset_sec = 3600; // Zmiana czasu
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-// Funkcja zwracaj�ca kolor dla cyfry 0�9
+// Funkcja zwracajaca kolor dla cyfry 0-9
 uint32_t resistorColor(int digit) {
   switch (digit) {
     case 0: return strip.Color(0, 0, 0);               // czarny
@@ -69,18 +69,15 @@ void loop() {
 
   strip.clear();
 
-  // Mapa LED:
-  // 0=h1, 1=h2, 2=spacer, 3=m1, 4=m2, 5=spacer, 6=s1, 7=s2
-
   strip.setPixelColor(0, resistorColor(h1));
   strip.setPixelColor(1, resistorColor(h2));
 
-  strip.setPixelColor(2, strip.Color(0, 0, 0)); // spacer
+  strip.setPixelColor(2, strip.Color(0, 0, 0)); // spacja
 
   strip.setPixelColor(3, resistorColor(m1));
   strip.setPixelColor(4, resistorColor(m2));
 
-  strip.setPixelColor(5, strip.Color(0, 0, 0)); // spacer
+  strip.setPixelColor(5, strip.Color(0, 0, 0)); // spacja
 
   strip.setPixelColor(6, resistorColor(s1));
   strip.setPixelColor(7, resistorColor(s2));
@@ -89,3 +86,4 @@ void loop() {
   delay(200);
 }
    
+
