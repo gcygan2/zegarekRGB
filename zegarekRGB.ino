2407ef5c30b653f56fe3817855404d290877238d
gcygan2 <114261165+gcygan2@users.noreply.github.com>
2026-01-18 21:33:10 +0100

    Add files via upload

---
+++ zegarekRGB.ino
@@ -0,0 +1,157 @@
+#include <WiFi.h>
+#include <Adafruit_NeoPixel.h>
+#include "time.h"
+#include <EEPROM.h>
+
+#define LED_PIN 5
+#define LED_COUNT 8
+
+const int EEPROM_SIZE = 512;
+const int START_ADDR = 0;
+
+String ssid;
+String password;
+
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 3600;      // Polska UTC+1
+const int   daylightOffset_sec = 3600; // Zmiana czasu
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+int tik = 0;
+
+// Funkcja zwracajaca kolor dla cyfry 0 - 9
+uint32_t resistorColor(int digit) {
+  switch (digit) {
+    case 0: return strip.Color(0, 0, 0);               // czarny
+    case 1: return strip.Color(80, 20, 10);           // brazowy
+    case 2: return strip.Color(255, 0, 0);             // czerwony
+    case 3: return strip.Color(255, 40, 0);           // pomaranczowy
+    case 4: return strip.Color(255, 100, 0);            // zolty
+    case 5: return strip.Color(0, 255, 0);             // zielony
+    case 6: return strip.Color(0, 0, 255);             // niebieski
+    case 7: return strip.Color(128, 0, 148);           // fioletowy
+    case 8: return strip.Color(20, 20, 20);         // szary
+    case 9: return strip.Color(255, 255, 255);         // bialy
+  }
+  return strip.Color(0, 0, 0);
+}
+
+void writeStringToEEPROM(int addr, const String &data) {
+  int len = data.length();
+  EEPROM.write(addr, len);  // zapisujemy długość
+  for (int i = 0; i < len; i++) {
+    EEPROM.write(addr + 1 + i, data[i]);
+  }
+}
+
+String readStringFromEEPROM(int addr) {
+  int len = EEPROM.read(addr);
+  if (len <= 0 || len > 250) return "";
+
+  String result = "";
+  for (int i = 0; i < len; i++) {
+    result += char(EEPROM.read(addr + 1 + i));
+  }
+  return result;
+}
+
+String readSerialWithTrigger() {
+  unsigned long start = millis();
+  Serial.println("Wcisnij dowolny klawisz, aby skonfigurowac siec WiFi.");
+  while (!Serial.available()) {
+    if (millis() - start > 2000) {
+      return "";
+    }
+    delay(5);
+  }
+
+  char trigger = Serial.read();
+
+  while (Serial.available()) {
+    Serial.read();
+  }
+
+  Serial.println("Wpisz dane sieci w formacie:<ssid>\\<haslo>. Masz na to 30 s.");
+
+  start = millis();
+  while (!Serial.available()) {
+    if (millis() - start > 30000) {
+      return "";
+    }
+    delay(5);
+  }
+
+  String input = Serial.readStringUntil('\n');
+  input.trim();
+
+  return input;
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  EEPROM.begin(EEPROM_SIZE);
+
+  String input = readSerialWithTrigger();
+  if (input.length() > 0) {
+    input.trim();
+    Serial.println(input);
+    writeStringToEEPROM(START_ADDR, input);
+    EEPROM.commit();
+  }
+
+  strip.begin();
+  strip.show();
+  String stored = readStringFromEEPROM(START_ADDR);
+
+  int sep = stored.indexOf('\\');
+  if (sep != -1) { 
+    ssid = stored.substring(0, sep);
+    password = stored.substring(sep + 1);
+  }
+  Serial.println(ssid);
+  Serial.println(password);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(300);
+  }
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+}
+
+void loop() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    return;
+  }
+
+  int hour   = timeinfo.tm_hour;
+  int minute = timeinfo.tm_min;
+
+  // Rozbijanie na cyfry
+  int h1 = hour / 10;
+  int h2 = hour % 10;
+
+  int m1 = minute / 10;
+  int m2 = minute % 10;
+
+  strip.clear();
+
+  strip.setPixelColor(0, resistorColor(h1));
+  strip.setPixelColor(2, resistorColor(h2));
+
+  if (tik) {
+    tik = 0;
+    strip.setPixelColor(3, strip.Color(0, 0, 0));
+    strip.setPixelColor(4, strip.Color(0, 0, 0));
+  } else {
+    tik = 1;
+    strip.setPixelColor(3, strip.Color(20, 20, 60));
+    strip.setPixelColor(4, strip.Color(20, 20, 60));
+  }
+
+  strip.setPixelColor(5, resistorColor(m1));
+  strip.setPixelColor(7, resistorColor(m2));
+
+  strip.show();
+  delay(500);
+}
